81a0788b93ecc28cdfc61fae689c08a20690885f
Saksham Chaturvedi <sakshamc2001@gmail.com>
2021-10-11 21:17:38 +0530

    redis upstash connect

--- database/redis.js
+++ database/redis.js
@@ -1,10 +1,13 @@
 var redis = require("redis");
 var session = require("express-session");
 var connectRedis = require("connect-redis");
+const { redis_host, redis_port, redis_password } = require("../config/index");
+
 const RedisStore = connectRedis(session);
 const redisClient = redis.createClient({
-  host: "localhost",
-  port: "6379",
+  host: redis_host,
+  port: redis_port,
+  password: redis_password,
 });
 
 redisClient.on("connect", () => {
